f37136cdf9a33a889f381a6174d9d65ef59b5352
Jeferson Soares <jpadilha53@gmail.com>
2024-07-15 18:25:45 -0300

    exercicio TS

---
+++ main.ts
@@ -0,0 +1,11 @@
+function multiplicacao(numA: number, numB: number): number{
+  return numA * numB
+}
+
+console.log(multiplicacao(3,9));
+
+function saudacao(nome: string):string {
+  return `Olá ${nome}`
+}
+
+console.log(saudacao("Jeferson"));
